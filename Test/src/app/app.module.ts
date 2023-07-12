df99ad63674539d3fe2178b3dde1671193878950
EdwardMOMalley <edwardomalley@unomaha.edu>
2023-07-12 11:33:16 -0500

    Update app.module.ts

--- Test/src/app/app.module.ts
+++ Test/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
   declarations: [
     AppComponent,
     UserSetupComponent,
+    ShoppingListComponent,
   ],
   imports: [
     BrowserModule,
