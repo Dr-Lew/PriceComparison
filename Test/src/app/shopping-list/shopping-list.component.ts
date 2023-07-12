2d4613c94fd2107ad85cf33b65a0b8ee68266a4a
ITompkins100 <135860537+ITompkins100@users.noreply.github.com>
2023-07-11 23:24:26 -0500

    Fix the search to show all products if search box is empty.

--- Test/src/app/shopping-list/shopping-list.component.ts
+++ Test/src/app/shopping-list/shopping-list.component.ts
@@ -20,6 +20,7 @@ export class ShoppingListComponent {
     { name: 'bread', count: 0 }
   ];
   shoppingList: Product[] = [];
+  filteredProductsList = this.products; // Set the filtered products = to all of the products.
 
   addToShoppingList(product: Product): void {
     const existingProduct = this.shoppingList.find(p => p.name === product.name);
@@ -33,8 +34,12 @@ export class ShoppingListComponent {
 
   submit() {
     //console.log(this.searchQuery);
-    const matchingProduct = this.products.find(product => product.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
-    this.products = matchingProduct ? [matchingProduct] : [];
+    if (this.searchQuery) { // If a search term was entered, set filtered products to the matching products found.
+      const matchingProduct = this.products.find(product => product.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+      this.filteredProductsList = matchingProduct ? [matchingProduct] : [];
+    } else { // If no search term was entered and it was blank, then set the filtered products = this.products which is all of them.
+      this.filteredProductsList = this.products;
+    }
   }
   
 }
