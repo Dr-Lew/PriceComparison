44de7bc176cf8b2546ba167c3a53fa7abc2b94d1
Dr-Lew <dalawrence210109@gmail.com>
2023-07-20 09:54:28 -0500

    Added documentation to shopping-cart.component.ts located in app/shopping-cart.

--- Test/src/app/shopping-cart/shopping-cart.component.ts
+++ Test/src/app/shopping-cart/shopping-cart.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
+
+/**
+ * The ShoppingCartComponent class represents the shopping cart page of our application. It is responsible for
+ * displaying the contents of the user's shopping cart to the user, and making changes to their shopping if
+ * the user needs to.
+ */
 export class ShoppingCartComponent {
 
 }
