dece3d62f25999b2e7834140285bc96df276eaa6
Dr-Lew <dalawrence210109@gmail.com>
2023-07-19 23:23:48 -0500

    Changed the field name groceryList to groceryCart. Added a method to add a product to the grocery cart.

--- Test/src/app/models/user.model.ts
+++ Test/src/app/models/user.model.ts
@@ -1,14 +1,18 @@
 import {Store} from "./store.model";
-import {Item} from "./item.model";
+import {Product} from "./product.model";
 
 export class User {
   stores: Store[] = [];
-  groceryList: Item[] = [];
+  groceryCart: Product[] = [];
 
   constructor(stores: Store[]) {
     this.stores = stores;
   }
   addStore(store: Store) {
     this.stores.push(store);
+  }
+
+  addProductToCart(product: Product) {
+    this.groceryCart.push(product);
   }
 }
