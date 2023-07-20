b32c476b59818793c062ca7ba928a803bfa01353
Dr-Lew <dalawrence210109@gmail.com>
2023-07-19 23:18:09 -0500

    Update Database.ts to use the name Product to reflect that item.model.ts was changed to product.model.ts. Also created a new service called DatabaseService and copied the code from Database.ts into DatabaseService.

---
+++ Test/src/app/services/database.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Product} from "../models/product.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+
+  prodDb: Product[] = [];
+  constructor(){
+    this.prodDb.push(new Product("Coca-Cola 12Pk","049000028904"));
+    this.prodDb.push(new Product("Diet Coke 12Pk","049000028911"));
+    this.prodDb.push(new Product("Diet Coke 24Pk","049000046571"));
+  }
+
+  getDatabaseofItems(): Product[]{
+    return this.prodDb;
+  }
+}
--- Test/src/assets/Database.ts
+++ Test/src/assets/Database.ts
@@ -1,16 +1,16 @@
-import { Item } from "src/app/models/item.model";
+import { Product } from "src/app/models/product.model";
 export class Database{
-   itemDb: Item[] = [];
+   itemDb: Product[] = [];
    constructor(){
-    this.itemDb.push(new Item("Coca-Cola 12Pk","049000028904"));
-    this.itemDb.push(new Item("Diet Coke 12Pk","049000028911"));
-    this.itemDb.push(new Item("Diet Coke 24Pk","049000046571"));
+    this.itemDb.push(new Product("Coca-Cola 12Pk","049000028904"));
+    this.itemDb.push(new Product("Diet Coke 12Pk","049000028911"));
+    this.itemDb.push(new Product("Diet Coke 24Pk","049000046571"));
    }
 
 
-   getDatabaseofItems(): Item[]{
+   getDatabaseofItems(): Product[]{
     return this.itemDb;
 
    }
-    
+
 }
