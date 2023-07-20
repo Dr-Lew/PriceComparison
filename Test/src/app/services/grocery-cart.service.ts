898da30fdbd1cce408797b7a85469f8bd4bed784
Dr-Lew <dalawrence210109@gmail.com>
2023-07-20 10:06:29 -0500

    Added a grocery cart service.

---
+++ Test/src/app/services/grocery-cart.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+/**
+ * The GroceryCartService provides useful functions for acting upon a cart of products.
+ */
+export class GroceryCartService {
+
+  constructor() { }
+}
