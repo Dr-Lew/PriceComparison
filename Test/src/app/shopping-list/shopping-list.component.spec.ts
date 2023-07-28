b79a051d13640c5ee95104fe11d2688f0b5d4a4b
EdwardMOMalley <edwardomalley@unomaha.edu>
2023-07-27 19:23:26 -0500

    Fixed tests

--- Test/src/app/shopping-list/shopping-list.component.spec.ts
+++ Test/src/app/shopping-list/shopping-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShoppingListComponent } from './shopping-list.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 describe('ShoppingListComponent', () => {
   let component: ShoppingListComponent;
@@ -8,7 +10,8 @@ describe('ShoppingListComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ShoppingListComponent]
+      declarations: [ShoppingListComponent],
+      imports: [MatGridListModule,FormsModule,ReactiveFormsModule]
     });
     fixture = TestBed.createComponent(ShoppingListComponent);
     component = fixture.componentInstance;
@@ -17,5 +20,9 @@ describe('ShoppingListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should TEST', () => {
+    spyOn
   });
 });
