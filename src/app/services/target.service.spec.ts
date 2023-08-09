405d1aad5bdb316a7f739a0d5a6b91181ef15618
EdwardMOMalley <edwardomalley@unomaha.edu>
2023-08-09 12:11:34 -0500

    Final push for now

--- src/app/services/target.service.spec.ts
+++ src/app/services/target.service.spec.ts
@@ -26,7 +26,7 @@ describe('TargetService', () => {
     expect(testStores).toContain(testStore);
   });
 
- // it('getCost() should return accurate pricing', async () => {
-   // expect( await service.getCost("049000028904","530")).toEqual(7.49);
- // });
+  it('getCost() should return accurate pricing', async () => {
+    expect( await service.getCost("049000028904","530")).toEqual(7.49);
+  });
 });
